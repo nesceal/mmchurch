1dd0a493971ca8498f65a9d52de175b89bd9ae47
Nesceal <nesceal@gmail.com>
2021-01-02 21:45:57 -0800

    Added logic for switching locales

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../header';
 import Section from '../section';
 import Footer from '../footer';
+import LocalizationData from '../../data/localization';
+import { LOCALE_KEY } from '../../util/constants';
 
 function App() {
+
+  const locale = localStorage.getItem(LOCALE_KEY) || 'english';
+  const [data, setData] = useState(LocalizationData[locale]);
+
   return (
     <main className="app">
       <Header />
       <Section />
-      <Footer />
+      <Footer locale={locale} setData={setData} />
     </main>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import LocalizationData from '../../data/localization';
 import { css } from 'aphrodite/no-important';
 import { styleSheet } from './styles';
+import { LOCALE_KEY } from '../../util/constants';
 
-const Footer = () => {
+const Footer = props => {
 
   const styles = styleSheet();
 
+  const handleLocaleClick = locale => {
+    if (props.locale !== locale) {
+      localStorage.setItem(LOCALE_KEY, locale);
+      props.setData(LocalizationData[locale]);
+    }
+  };
+
   return (
     <footer className={css(styles.footer)}>
-      Footer
+      <div className={css(styles.locale)}>
+        <span onClick={() => handleLocaleClick('english')}>English</span> | <span onClick={() => handleLocaleClick('tagalog')}>Filipino</span>
+      </div>
     </footer>
   );
 };
