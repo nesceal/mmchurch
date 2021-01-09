b2d73a76179c9e324f1382937536b3b9a01487ea
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2021-01-08 21:07:22 -0800

    Updated header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,29 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from 'aphrodite/no-important';
 import { styleSheet } from './styles';
 
 const Header = ({ data }) => {
   const styles = styleSheet();
   const { about, history, events, beliefs, contact } = data;
+  const [selectedNavItem, setSelectedNavItem] = useState('');
+
+  const handleNavItemClick = (navItem) => {
+    setSelectedNavItem(navItem);
+    // setSelectedNavItem(item);
+  };
 
   return (
-    <header className={css(styles.header)}>
-      <a href="#about" className={css(styles.headerItem)}>
-        {about.header}
-      </a>
-      <a href="#history" className={css(styles.headerItem)}>
-        {history.header}
-      </a>
-      <a href="#events" className={css(styles.headerItem)}>
-        {events.header}
-      </a>
-      <a href="#beliefs" className={css(styles.headerItem)}>
-        {beliefs.header}
-      </a>
-      <a href="#contact" className={css(styles.headerItem)}>
-        {contact.header}
-      </a>
-    </header>
+    <nav className={css(styles.nav)}>
+      <ul className={css(styles.list, styles.leftList)}>
+        <li
+          className={css(
+            styles.listItem,
+            selectedNavItem === 'about' && styles.active
+          )}
+          onClick={() => handleNavItemClick('about')}
+        >
+          <a href="#about" className={css(styles.label)}>
+            {about.header}
+          </a>
+        </li>
+        <li
+          className={css(
+            styles.listItem,
+            selectedNavItem === 'history' && styles.active
+          )}
+          onClick={() => handleNavItemClick('history')}
+        >
+          <a href="#history" className={css(styles.label)}>
+            {history.header}
+          </a>
+        </li>
+        <li
+          className={css(
+            styles.listItem,
+            styles.leftListLastItem,
+            selectedNavItem === 'events' && styles.active
+          )}
+          onClick={() => handleNavItemClick('events')}
+        >
+          <a href="#events" className={css(styles.label)}>
+            {events.header}
+          </a>
+        </li>
+      </ul>
+      <div className={css(styles.logoContainer)}>
+        <img src="logo.png" className={css(styles.logo)}></img>
+      </div>
+      <ul className={css(styles.list, styles.righttList)}>
+        <li
+          className={css(
+            styles.listItem,
+            styles.rightListfirstItem,
+            selectedNavItem === 'beliefs' && styles.active
+          )}
+          onClick={() => handleNavItemClick('beliefs')}
+        >
+          <a href="#beliefs" className={css(styles.label)}>
+            {beliefs.header}
+          </a>
+        </li>
+        <li
+          className={css(
+            styles.listItem,
+            selectedNavItem === 'contact' && styles.active
+          )}
+          onClick={() => handleNavItemClick('contact')}
+        >
+          <a href="#contact" className={css(styles.label)}>
+            {contact.header}
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,18 +1,67 @@
 import { StyleSheet } from 'aphrodite/no-important';
+import { STYLE_MEDIA } from '../../util/constants';
 
 export const styleSheet = () =>
   StyleSheet.create({
-    header: {
+    nav: {
+      backgroundColor: '#800000',
+      position: 'sticky',
+      top: '0',
+      minWidth: '760px',
+    },
+    list: {
+      width: '50%',
+      display: 'inline-flex',
+      listStyleType: 'none',
+      padding: '0',
+    },
+    leftList: {
+      justifyContent: 'flex-end',
+    },
+    righttList: {
+      justifyContent: 'flex-start',
+    },
+    listItem: {
       display: 'flex',
       justifyContent: 'center',
-      overflow: 'hidden',
-      backgroundColor: '#800000',
-    },
-    headerItem: {
-      color: '#ffffff',
-      textAlign: 'center',
       padding: '14px',
+      minWidth: '12.5%',
+    },
+    leftListLastItem: {
+      marginRight: '20%',
+    },
+    rightListfirstItem: {
+      marginLeft: '20%',
+    },
+    label: {
+      color: '#ffffff',
+      fontWeight: '700',
       textDecoration: 'none',
       textTransform: 'uppercase',
+    },
+    logoContainer: {
+      position: 'absolute',
+      backgroundColor: '#800000',
+      width: '155px',
+      borderRadius: '50%',
+      height: '155px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      left: '0',
+      right: '0',
+      top: '0',
+    },
+    logo: {
+      position: 'absolute',
+      top: '12px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      left: '0',
+      right: '0',
+      height: '100px',
+    },
+    active: {
+      backgroundColor: '#3B850D',
+      borderRadius: '4px',
     },
   });
