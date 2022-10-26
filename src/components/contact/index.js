8702b8f7610532dece9ca6c7defef25d7d67be84
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2022-10-25 19:19:58 -0700

    Updated Contact Us section

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -3,13 +3,15 @@ import { css } from 'aphrodite/no-important';
 import { styleSheet } from './styles';
 
 const Contact = (props) => {
-
   const styles = styleSheet();
 
   return (
-    <section id='contact' className={css(styles.contact)}>
+    <section id="contact" className={css(styles.contact)}>
       <h1 className={css(styles.headline)}>{props.data.header}</h1>
-      <p className={css(styles.paragraph)} dangerouslySetInnerHTML={{ __html: props.data.contents }} />
+      <p
+        className={css(styles.paragraph)}
+        dangerouslySetInnerHTML={{ __html: props.data.contents }}
+      />
     </section>
   );
 };
